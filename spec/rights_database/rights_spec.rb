1f4039804303eaea9101d412db6012380c97904d
Josh Steverman <jstever@umich.edu>
2022-03-18 17:11:18 -0400

    Use db-image test data

--- spec/rights_database/rights_spec.rb
+++ spec/rights_database/rights_spec.rb
@@ -6,9 +6,9 @@ require "rights_database/rights"
 RSpec.describe RightsDatabase::Rights do
   describe "#new" do
     it "retrieves a rights record for an item id" do
-      item_rights = RightsDatabase::Rights.new(item_id: "uva.x001592955")
+      item_rights = RightsDatabase::Rights.new(item_id: "test.cc-by-nc-nd-4.0_page")
       expect(item_rights.attribute.id).to eq(22)
-      expect(item_rights.reason.id).to eq(3)
+      expect(item_rights.reason.id).to eq(1)
     end
   end
 end
