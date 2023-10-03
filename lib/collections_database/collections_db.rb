740c7de2d044678b56296a5999cbda1353081789
Aaron Elkiss <aelkiss@umich.edu>
2023-10-03 12:47:39 -0400

    Update dependencies
    - ruby 3.2
    - address standardrb concerns

--- lib/collections_database/collections_db.rb
+++ lib/collections_database/collections_db.rb
@@ -13,8 +13,8 @@ module CollectionsDatabase
     attr_reader :rawdb
     attr_accessor :connection_string
 
-    def initialize(connection_string = ENV["DB_CONNECTION_STRING"], **kwargs)
-      @rawdb = self.class.connection(connection_string, **kwargs)
+    def initialize(connection_string = ENV["DB_CONNECTION_STRING"], **)
+      @rawdb = self.class.connection(connection_string, **)
       super(@rawdb)
     end
 
@@ -43,8 +43,6 @@ module CollectionsDatabase
       else
         Sequel.connect(connection_string)
       end
-    rescue Sequel::DatabaseConnectionError => e
-      raise e
     end
 
     class << self
