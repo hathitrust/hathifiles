ae97bbb8db21c493cc8cbe25c6009852e764d4d6
Brian "Moses" Hall <moseshll@umich.edu>
2026-08-10 11:49:22 -0400

    Dependency updates for August 2026 - `bundle update --bundler` - `bundle update --all` - Update `hathitrust_contrib_configs` submodule to latest - Fix indentation issue in `HathifileWriter` newly uncovered by `standardrb` - Fix `SimpleCov` `add_filter` deprecation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require "services"
 require "simplecov"
 require "simplecov-lcov"
 
-SimpleCov.add_filter "spec"
+SimpleCov.skip "spec"
 
 SimpleCov::Formatter::LcovFormatter.config do |c|
   c.report_with_single_file = true
